8431f9869f99d24140a8e36659a6546cb415c79a
khursandov <sarvar@ips-dev.com>
2022-07-08 23:03:33 +0500

    created tag module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TagController } from './tag/tag.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, TagController],
   providers: [AppService],
 })
 export class AppModule {}
